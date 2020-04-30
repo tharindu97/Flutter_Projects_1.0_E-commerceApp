b4002ab8868449ccd08f019f2b5bad02e8afd4a6
tharindu97 <tharindukavishna@gmail.com>
2020-04-30 20:44:08 +0530

    13_completed

--- lib/componets/products.dart
+++ lib/componets/products.dart
@@ -6,10 +6,39 @@ class Products extends StatefulWidget {
 }
 
 class _ProductsState extends State<Products> {
+  var product_list =[
+    {
+      "name": "Blazer",
+      "picture":"images/c2.png",
+      "old_price":120,
+      "price":85
+    },
+    {
+      "name": "Blazer",
+      "picture":"images/c2.png",
+      "old_price":120,
+      "price":85
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       
+    );
+  }
+}
+
+class Single_prod extends StatelessWidget {
+  
+  final prod_name;
+  final prod_picture;
+  final prod_old_price;
+  final prod_price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     
     );
   }
 }
