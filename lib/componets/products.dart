8a6916131a97b982dce2647e3a23a3bd960b7773
tharindu97 <tharindukavishna@gmail.com>
2020-05-03 11:05:43 +0530

    have_bug

--- lib/componets/products.dart
+++ lib/componets/products.dart
@@ -58,7 +58,12 @@ class Single_prod extends StatelessWidget {
         tag: prod_name, 
         child: Material(
           child: InkWell(
-            onTap: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProductDetails())),
+            onTap: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProductDetails(
+                product_detail_name: prod_name,
+                product_detail_new_price: prod_price,
+                product_detail_old_price:prod_old_price,
+                product_detail_picture:  prod_picture
+            ))),
             child: GridTile(
               footer: Container(
                 color: Colors.white70,
--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
+  final product_detail_name;
+  final product_detail_new_price;
+  final product_detail_old_price;
+  final product_detail_picture;
+
+  ProductDetails({
+    this.product_detail_name,
+    this.product_detail_new_price,
+    this.product_detail_old_price,
+    this.product_detail_picture
+  });
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
 }
@@ -8,6 +19,21 @@ class ProductDetails extends StatefulWidget {
 class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+       appBar:  AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Fashapp'),
+        actions: <Widget>[
+           IconButton(
+            icon: Icon(Icons.search, color:Colors.white),
+            onPressed: (){},
+          ),
+           IconButton(
+            icon: Icon(Icons.shopping_cart, color:Colors.white),
+            onPressed: (){},
+          )
+        ],
+      ),
+    );
   }
 }
